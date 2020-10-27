bc793d139b98b8cc08cb6fa4232c344953bd7de1
marijus_tumosa <marijus@zenitech.co.uk>
2020-10-27 10:40:01 +0200

    eslint and env fixes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 app.listen(port, () => {
-  console.log(`App listening at port ${port}`);
-})
+  console.info(`App listening at port ${port}`);
+});
