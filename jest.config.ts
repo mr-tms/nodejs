4bb4412e9199b65137a22090b5242be7c6580e63
marijus_tumosa <marijus@zenitech.co.uk>
2021-03-03 17:33:31 +0200

    PR5: initial jest config

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  verbose: true,
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    '/node_modules/'
+  ],
+};
+export default config;
---
+++ src/__tests__/api.test.ts
@@ -0,0 +1,8 @@
+import { Request } from 'supertest';
+import app from '../app';
+
+describe('api endpoints', () => {
+  it('should create new entry', () => {
+    expect(true).toBe(true)
+  })
+})
