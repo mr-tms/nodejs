a0262335034c9ea9187fbbc9329da61a8a05c58c
marijus_tumosa <marijus@zenitech.co.uk>
2021-03-05 15:00:24 +0200

    PR5: working mocha setup

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,13 +1,12 @@
-import assert from 'assert';
+// import assert from 'assert';
 import supertest from 'supertest';
 import app from '../src/app';
 
 const baseUrl = 'http://localhost:8000/api/v1';
 
-
 describe('GET /users', () => {
   it('should get all database entries', async () => {
     const response = await supertest(app).get(`${baseUrl}/users`);
-
+    console.log(response);
   });
 });
