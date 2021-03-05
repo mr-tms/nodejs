5d879efe58fd753242dd6d8c557069e816d232f5
marijus_tumosa <marijus@zenitech.co.uk>
2021-03-05 16:29:29 +0200

    PR5: GET users test

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,12 +1,30 @@
 // import assert from 'assert';
 import supertest from 'supertest';
 import app from '../src/app';
+import mongoose from 'mongoose';
+import userModel from '../src/models/userModel';
 
-const baseUrl = 'http://localhost:8000/api/v1';
+beforeEach((done) => {
+  mongoose.connect("mongodb://localhost:27017/TestDB",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => done());
+});
+
+afterEach((done) => {
+  mongoose.connection.db.dropDatabase(() => {
+    mongoose.connection.close(() => done())
+  });
+});
 
 describe('GET /users', () => {
   it('should get all database entries', async () => {
-    const response = await supertest(app).get(`${baseUrl}/users`);
-    console.log(response);
+    await userModel.create({
+      firstName: 'Agent',
+      lastName: 'Smith',
+      email: 'agentSmith@matrix.org'
+    });
+
+    const response = await supertest(app).get('/api/v1/users');
+    console.log(response.body);
   });
 });
